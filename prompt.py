63ebc0ef532964dbd9438b9b5d9f5a954e604811
Martynas <martynas.j@oxylabs.io>
2023-12-21 10:34:52 +0200

    Update prompt

--- prompt.py
+++ prompt.py
@@ -4,20 +4,25 @@ import utils
 
 PROMPT_ = """
 User
-I will provide you an order id, an email subject and a body from online shop . Please answer two questions:
+I will provide you an order_id, an email subject and a body from online shop . Please answer two questions:
 - pick status from:
         0 – payment confirmation;
         1 – order confirmation or order is ready to be shipped;
         2 – order shipped;
         3 – order cancelation;
         4 – client service response;
-- find tracking id/number. If you find two or more tracking ids, format it as a list;
+- find tracking id/number. If you find two or more tracking ids, format it as a list, tracking number should be be the same as provided order_id
 
 order_id: {order_id};
 subject: {subject};
 body: {email_text}.
 
 Only answer in JSON, without any explanation. Copy tracking URL from provided body. JSON example:
+{{
+  "status": 2,
+  "tracking_id": "TW 54321384923",
+}}
+or
 {{
   "status": 2,
   "tracking_id": "54321",
--- test_external_id.py
+++ test_external_id.py
@@ -6,6 +6,8 @@ from logging.config import dictConfig
 import click
 
 import ms_api
+import utils
+import store
 from run import get_emails, process_email
 from logger import LOGGING_CONFIG
 
@@ -15,7 +17,7 @@ logger = logging.getLogger(__name__)
 DAYS_TO_CHECK_EMAIL = 30
 
 
-async def run_external_id(external_id: str, save: int) -> None:
+async def run_external_id(external_id: str, save: int, send: int) -> None:
     access_token = await ms_api.get_access_token()
     order_id = str(external_id[:-3])
     emails = await get_emails(order_id, access_token, days=DAYS_TO_CHECK_EMAIL)
@@ -30,14 +32,24 @@ async def run_external_id(external_id: str, save: int) -> None:
                 json.dump(email, f, indent=2)
         open_api_json = await process_email(order_id, email)
         logger.info(f"Response from openai: `{open_api_json}`.")
+        if send:
+            await store.store_response(
+                open_api_json=open_api_json,
+                external_id=external_id,
+                email_date=utils.convert_iso_to_mysql_datetime(
+                    email["receivedDateTime"]
+                ),
+                email_id=email["id"],
+            )
 
 
 @click.command()
 @click.option("--external_id", required=True)
 @click.option("--save", required=False, type=int, default=0)
-def main(external_id: str, save) -> None:
+@click.option("--send", required=False, type=int, default=0)
+def main(external_id: str, save: int, send: int) -> None:
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_external_id(external_id, save))
+    loop.run_until_complete(run_external_id(external_id, save, send))
 
 
 if __name__ == "__main__":
